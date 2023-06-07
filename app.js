e69d104d3fb6c1d5b8ddbaa52d96590152d9893f
James koduah <101557545+James-koduah@users.noreply.github.com>
2023-06-07 17:03:28 +0000

    initial

---
+++ app.js
@@ -0,0 +1,5 @@
+function l(){
+    document.documentElement.style.setProperty('--main-bg', 'rgb(249, 246, 240)')
+    document.documentElement.style.setProperty('--borders', 'rgb(0, 0, 0)')
+    document.documentElement.style.setProperty('--shadow', 'rgb(0, 0, 0)')
+}
