18f38ddf05295b0c6f51a995704b4aff495268cf
James-koduah <jameskoduah171@gmail.com>
2023-06-07 17:14:16 +0000

    lightmode dark mode

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+let light = False
+
 function l(){
-    document.documentElement.style.setProperty('--main-bg', 'rgb(249, 246, 240)')
-    document.documentElement.style.setProperty('--borders', 'rgb(0, 0, 0)')
-    document.documentElement.style.setProperty('--shadow', 'rgb(0, 0, 0)')
+	if (light == False)
+	{
+		document.documentElement.style.setProperty('--main-bg', 'rgb(249, 246, 240)')
+		document.documentElement.style.setProperty('--borders', 'rgb(0, 0, 0)')
+		document.documentElement.style.setProperty('--shadow', 'rgb(0, 0, 0)')
+		light = True
+	}
+	if (light == True){
+		 document.documentElement.style.setProperty('--main-bg', 'rgb(33, 31, 31')
+		 document.documentElement.style.setProperty('--main-bg', 'wheat')
+		 document.documentElement.style.setProperty('--main-bg', 'white')
+	}
 }
