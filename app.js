44fd4d6bc952fc8f955cc90077a5e67faedd90e7
James-koduah <jameskoduah171@gmail.com>
2023-06-08 09:44:30 +0000

    app.js

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-let light = False
-
+let light = false;
 function l(){
-	if (light == False)
-	{
-		document.documentElement.style.setProperty('--main-bg', 'rgb(249, 246, 240)')
-		document.documentElement.style.setProperty('--borders', 'rgb(0, 0, 0)')
-		document.documentElement.style.setProperty('--shadow', 'rgb(0, 0, 0)')
-		light = True
-	}
-	if (light == True){
-		 document.documentElement.style.setProperty('--main-bg', 'rgb(33, 31, 31')
-		 document.documentElement.style.setProperty('--main-bg', 'wheat')
-		 document.documentElement.style.setProperty('--main-bg', 'white')
-	}
+    if (light == false){
+        document.documentElement.style.setProperty('--main-bg', 'rgb(249, 246, 240)')
+        document.documentElement.style.setProperty('--borders', 'rgb(0, 0, 0)')
+        document.documentElement.style.setProperty('--shadow', 'rgb(0, 0, 0)')
+        light = true
+    }
+    else
+    if (light == true){
+        document.documentElement.style.setProperty('--shadow', 'white')
+        document.documentElement.style.setProperty('--main-bg', 'rgb(33, 31, 31)')
+        document.documentElement.style.setProperty('--borders', 'wheat')
+        light = false
+    }
 }
