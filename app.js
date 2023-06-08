1da2ddfb45e53585263ac3b0d3126f02558f0e44
James-koduah <jameskoduah171@gmail.com>
2023-06-08 10:21:05 +0000

    up

--- app.js
+++ app.js
@@ -14,3 +14,22 @@ function l(){
         light = false
     }
 }
+
+
+let task_info = document.getElementById('task_info')
+let ex = document.getElementById('ex')
+for (let i = 0; i < 400; i++){
+    ex.innerHTML += '----------------------------------------------------------------------------------------'
+}
+let tasks = document.getElementsByClassName('task')
+for (let i = 0; i < tasks.length; i++){
+    tasks[i].addEventListener('click', ()=>{
+        hh()
+    })
+}
+function hh(){
+    task_info.style.zIndex = 9
+}
+function c(){
+    task_info.style.zIndex = -9
+}
